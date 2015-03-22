b50ed2cb799e2547f2897ff4ef3faa9195b2b874
Bjørn Friese <bearfrieze@gmail.com>
2015-03-22 20:39:51 +0100

    Having fun with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,48 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             all: ['Gruntfile.js', 'src/*.js']
+        },
+        less: {
+            dev: {
+                files: {
+                    "dest/style.css": "src/style.less"
+                }
+            },
+            build: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "dest/style.css": "src/style.less"
+                }
+            }
+        },
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    src: ['src/*.html', 'src/*.js'],
+                    dest: 'dest/',
+                    filter: 'isFile',
+                    flatten: true
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/*.less'],
+                tasks: ['less:dev', 'copy:dev'],
+                options: {
+                    spawn: false
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('dev', ['jshint', 'less:dev', 'copy:dev']);
+    grunt.registerTask('build', ['less:build']);
+    grunt.registerTask('default', ['dev']);
 };
